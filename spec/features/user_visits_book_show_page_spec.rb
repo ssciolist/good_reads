8bf60b4f2857246c0d0cfe269a3232a0bf0c7085
Megan Arellano <ssciolist@gmail.com>
2018-03-30 10:58:13 -0600

    ok first user story

--- spec/features/user_visits_book_show_page_spec.rb
+++ spec/features/user_visits_book_show_page_spec.rb
@@ -18,17 +18,18 @@ describe 'As a user' do
         review_1 = book.reviews.create!(user_rating: 4, body: "I liked this well enough", user_id: user_1.id)
         review_2 = book.reviews.create!(user_rating: 3, body: "Needs more Fergie", user_id: user_2.id)
         review_3 = book.reviews.create!(user_rating: 4, body: "Beautiful", user_id: user_3.id)
+
         visit book_path(book)
 
-        expect(page).to have_content(user_1.name)
-        expect(page).to have_content(user_1.user_rating)
-        expect(page).to have_content(user_1.body)
-        expect(page).to have_content(user_2.name)
-        expect(page).to have_content(user_2.user_rating)
-        expect(page).to have_content(user_2.body)
-        expect(page).to have_content(user_3.name)
-        expect(page).to have_content(user_3.user_rating)
-        expect(page).to have_content(user_3.body)
+        expect(page).to have_content(user_1.id)
+        expect(page).to have_content(review_1.user_rating)
+        expect(page).to have_content(review_1.body)
+        expect(page).to have_content(user_2.id)
+        expect(page).to have_content(review_2.user_rating)
+        expect(page).to have_content(review_2.body)
+        expect(page).to have_content(user_3.id)
+        expect(page).to have_content(review_3.user_rating)
+        expect(page).to have_content(review_3.body)
       end
     end
 
