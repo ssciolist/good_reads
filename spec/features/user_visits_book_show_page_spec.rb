05e70006a7aef913642a1fc27d315d7ea4df92a4
Megan Arellano <ssciolist@gmail.com>
2018-03-30 10:16:41 -0600

    writes some test, rspec seems to not be working

---
+++ spec/features/user_visits_book_show_page_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  describe 'when I visit /books/:id' do
+    describe 'I see the book title' do
+    end
+
+    describe 'I see a list of reviews for that book' do
+      describe 'each review includes user, user rating, and review body' do
+      end
+    end
+  end
+end
