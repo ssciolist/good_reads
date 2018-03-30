9973f025d5517b6e10cc0798a6907960b1a8975e
Megan Arellano <ssciolist@gmail.com>
2018-03-30 10:40:05 -0600

    failing test

--- spec/features/user_visits_book_show_page_spec.rb
+++ spec/features/user_visits_book_show_page_spec.rb
@@ -10,6 +10,23 @@ describe 'As a user' do
     end
 
     describe 'I see a list of reviews for that book' do
+      it 'each review has user, review body and rating' do
+        book = Book.create!(title: "Great Gatsby")
+        review_1 = Review.create!(user: "Jake", user_rating: 4, body: "I liked this well enough")
+        review_2 = Review.create!(user: "Mike", user_rating: 3, body: "Needs more Fergie")
+        review_3 = Review.create!(user: "Laura", user_rating: 4, body: "Beautiful")
+        visit book_path(book)
+
+        expect(page).to have_content(review_1.user)
+        expect(page).to have_content(review_1.user_rating)
+        expect(page).to have_content(review_1.body)
+        expect(page).to have_content(review_2.user)
+        expect(page).to have_content(review_2.user_rating)
+        expect(page).to have_content(review_2.body)
+        expect(page).to have_content(review_3.user)
+        expect(page).to have_content(review_3.user_rating)
+        expect(page).to have_content(review_3.body)
+      end
     end
 
   end
