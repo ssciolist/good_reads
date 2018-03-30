4e13708f0d2611f9300e5f1b671d8c75ed98abe5
Megan Arellano <ssciolist@gmail.com>
2018-03-30 10:32:26 -0600

    finally moving, failing test, ali, I'm probs gonna skip model tests again, in order to get something on the page, sorry

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ApplicationRecord
-  
+  validates :title, presence: true
 end
--- spec/features/user_visits_book_show_page_spec.rb
+++ spec/features/user_visits_book_show_page_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe 'As a user' do
   describe 'when I visit /books/:id' do
-    describe 'I see the book title' do
+    it 'I see the book title' do
+      book = Book.create!(title: "Great Gatsby")
+      visit book_path(book)
+
+      expect(page).to have_content(book.title)
     end
 
     describe 'I see a list of reviews for that book' do
-      describe 'each review includes user, user rating, and review body' do
-      end
     end
+
   end
 end
